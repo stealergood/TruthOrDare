80ffd526e70f78e77e6b03abf8a45023a5406449
Denny Edy Saputra <ncstealer@gmail.com>
2023-05-03 13:05:48 +0700

    Changes to be committed: 	modified:   src/config/database.js

--- src/config/database.js
+++ src/config/database.js
@@ -2,6 +2,7 @@ import { Sequelize } from "sequelize";
 
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql',
 });
 
