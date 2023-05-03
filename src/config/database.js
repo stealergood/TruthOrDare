155fd875895100a5ae2986b6975ca1da1a8ce368
Denny Edy Saputra <ncstealer@gmail.com>
2023-05-03 12:09:08 +0700

    Changes to be committed: 	modified:   .env 	modified:   src/config/database.js

--- src/config/database.js
+++ src/config/database.js
@@ -3,8 +3,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const db = new Sequelize(process.env.MYSQLDATABSE, process.env.MYSQLUSERNAME, process.env.MYSQLPASSWORD, {
-    host: process.env.MYSQLHOST,
+    database: process.env.MYSQLDATABSE,
+    username: process.env.MYSQLUSERNAME,
+    password: process.env.MYSQLPASSWORD,
     dialect: 'mysql',
+    port: process.env.MYSQLPORT,
+    host: process.env.MYSQLHOST,
 });
 
 export default db;
